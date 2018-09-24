dba6acd2170c0da11b3276e7d75c4e0e60b12588
michaelcastro <michaelcastro.js@gmail.com>
2018-09-25 00:08:30 +0300

    format amt and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,23 +1,44 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { removeExpense } from '../actions/expenses'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
+
+numeral.register('locale', 'fr', {
+  delimiters: {
+      thousands: ' ',
+      decimal: ','
+  },
+  abbreviations: {
+      thousand: 'k',
+      million: 'm',
+      billion: 'b',
+      trillion: 't'
+  },
+  ordinal : function (number) {
+      return number === 1 ? 'er' : 'ème';
+  },
+  currency: {
+      symbol: '€'
+  }
+});
+
+// switch between locales
+numeral.locale('fr');
 
 const ExpenseListItem =( {description, amount, createdAt, id, dispatch } )=> (
   <div>
-  
     <Link to={`/edit/${id}`}>
       <p>{description}</p>
     </Link>
-    <p>{amount} - {createdAt}</p>
-    <button 
-      onClick={()=>{
-        dispatch(removeExpense({id}))
-      }}>Remove</button>
+    <p>
+      {numeral(amount/ 100).format('$0,0.00')} 
+      :
+      {moment(createdAt).format('Do MMMM, YYYY')}
+    </p>
   </div>
 )
 
-export default connect()(ExpenseListItem)
+export default ExpenseListItem
 
 // const ExpenseListItem =(props)=> (
 //   <div>
