254cb281bf4f08f2bc24802bd4def771e88183b8
michaelcastro <michaelcastro.js@gmail.com>
2018-10-05 13:32:51 +0300

    fb

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -2,23 +2,35 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { startLogin, startLoginFb } from '../actions/auth'
 
-export const LogIn = ({startLogin, startLoginFb})=>{
+export const LogIn = ({ startLogin, startLoginFb }) => {
   return (
     <div className='box-layout'>
       <div className='box-layout__box'>
         <h1 className='box-layout__title'>Expendify!</h1>
         <p>Let this app helps you start saving!</p>
-        <button onClick={startLogin} className='button'>LogIn with Google</button>
-        <button onClick={startLoginFb} className='button'>LogIn with FaceBook</button>
+        <div className='login-button'>
+          <button
+            onClick={startLogin}
+            className='button button--secondary'>
+            LogIn with Google
+            <img src='./images/gmail.png' />
+        </button>
+          <button
+            onClick={startLoginFb}
+            className='button button--white'>
+            LogIn with Facebook
+            <img src='./images/facebook.png' />
+        </button>
+        </div>
       </div>
-      
+
     </div>
   )
 }
 
-const mapDispatchToProps = (dispatch)=>({
-  startLogin: ()=> dispatch(startLogin()),
-  startLoginFb: ()=> dispatch(startLoginFb()),
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin()),
+  startLoginFb: () => dispatch(startLoginFb()),
 
 })
 
